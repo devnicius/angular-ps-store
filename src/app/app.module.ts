317d76909de42704d44a31514f9149539dd85c62
vinicius.oliveira <vinicius.oliveira@movemais.com>
2024-05-24 17:19:13 -0300

    feat: initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuMarComponent } from './components/menu-mar/menu-mar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    MenuMarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/menu-mar/menu-mar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-mar',
+  templateUrl: './menu-mar.component.html',
+  styleUrls: ['./menu-mar.component.css']
+})
+export class MenuMarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
