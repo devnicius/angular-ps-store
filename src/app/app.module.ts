8b6683a5c384282c58acc7021ca89e803c38650e
vinicius.oliveira <vinicius.oliveira@movemais.com>
2024-05-28 17:55:16 -0300

    feat: add structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CardComponent } from './components/card/card.component';
-import { MenuMarComponent } from './components/menu-mar/menu-mar.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     CardComponent,
-    MenuMarComponent
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
